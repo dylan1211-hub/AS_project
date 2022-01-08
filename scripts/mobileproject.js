3e08a3a603ada9f5eb529b95ebc8c96d01d8180b
dylan1211-hub <hsteven2538@gmail.com>
2022-01-08 21:27:00 +0800

    0108-4

--- scripts/mobileproject.js
+++ scripts/mobileproject.js
@@ -23,7 +23,13 @@ window.onload = () => {
          lat:22.99967413348661, 
          lon:120.21979499894803,
          building:"總圖"
-        }
+        },
+        {
+            name: '萬紅刀切麵',
+            lat:23.010148836235047,  
+            lon:120.22477253110293,
+            building:"總圖"
+        },
  
     ];
     renderPlaces(places);
@@ -125,6 +131,33 @@ function renderPlaces(places) {
     icon.setAttribute('name', places[i].name);      //修改後
     icon.setAttribute('building', places[i].building);
     icon.setAttribute('src','https://dylan1211-hub.github.io/AS_project/assets/r4.png');
+
+    icon.setAttribute('scale','10, 10');
+
+    icon.addEventListener('loaded', () => window.dispatchEvent(new CustomEvent('gps-entity-place-loaded')));
+
+
+    const title = document.createElement('a-text');
+    title.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
+    title.setAttribute('value', places[i].name);
+    title.setAttribute('scale', '20 20 20');
+    title.addEventListener('loaded', () => window.dispatchEvent(new CustomEvent('gps-entity-place-loaded')))
+
+    scene.appendChild(icon);
+    scene.appendChild(title);
+   };
+
+   for(let i=4;i<5;i++) {
+    const latitude = places[i].lat;  //修改後
+    const longitude = places[i].lon; //修改後
+
+    // add place icon 
+    const icon = document.createElement('a-image');
+    icon.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
+    //icon.setAttribute('name', place.name);         修改前
+    icon.setAttribute('name', places[i].name);      //修改後
+    icon.setAttribute('building', places[i].building);
+    icon.setAttribute('src','https://dylan1211-hub.github.io/AS_project/assets/s.png');
 
     icon.setAttribute('scale','10, 10');
 
